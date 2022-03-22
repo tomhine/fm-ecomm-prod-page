71d3233be0578e57cb673fcfbbdbd3d07a8a1b3b
tomhine792 <tomhine792@outlook.com>
2022-03-22 22:51:40 +0000

    changed lots over to vue

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Logo from "./icons/Logo.vue";
+import Nav from "./Navigation/Nav.vue";
+import MenuIcon from "./icons/MenuIcon.vue";
+import CartQuantity from "./CartQuantity.vue";
+</script>
+
+<template>
+  <header class="w-full h-17 lg:h-28 flex items-center justify-center">
+    <div
+      class="w-full lg:max-w-6xl h-full flex items-center justify-between border-b-2 px-6 lg:px-0"
+    >
+      <div class="flex items-center gap-4 lg:gap-12 h-full">
+        <MenuIcon />
+        <Logo />
+        <Nav />
+      </div>
+      <div class="flex items-center gap-6 lg:gap-10">
+        <CartQuantity />
+        <div class="p-0.5 rounded-full hover:bg-brand-orange cursor-pointer">
+          <img src="/images/image-avatar.png" alt="avatar profile image" class="h-8 lg:h-12" />
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/components/Navigation/Nav.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavItem from "./NavItem.vue";
+
+const links = ["Collections", "Men", "Women", "About", "Contact"];
+</script>
+
+<template>
+  <nav class="hidden lg:flex h-full">
+    <ul class="flex items-center gap-6 h-full text-gray-500 text-sm">
+      <NavItem v-for="link in links">{{ link }}</NavItem>
+    </ul>
+  </nav>
+</template>
