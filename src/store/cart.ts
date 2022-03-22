2344e34016a833aaa9c96d26d4af4f66072a28ba
tomhine792 <tomhine792@outlook.com>
2022-03-22 22:49:50 +0000

    simplify cart while chasing wild geese

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,18 +1,9 @@
 import { reactive } from "vue";
 
-const product = { name: "Fall Limited Edition Sneakers", price: 125 };
+const cart = reactive({ value: 0 });
 
-const count = reactive({ value: 0 });
-const cart = reactive({ product, quantity: 0 });
-
-const decrement = () => {
-  if (count.value - 1 < 1) return;
-  count.value--;
-};
-const increment = () => count.value++;
-
-const addToCart = (quantity: number = 1) => {
-  cart.quantity = quantity;
+const addToCart = (quantity: number) => {
+  cart.value += quantity;
 };
 
-export { count, cart, decrement, increment, addToCart };
+export { cart, addToCart };
