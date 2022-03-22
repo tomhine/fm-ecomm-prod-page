567423dd7f1800a5e7183b89d9deba8a1cd6cb2b
tomhine792 <tomhine792@outlook.com>
2022-03-22 22:59:42 +0000

    move to using ref, store count in localStorage

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,9 +1,14 @@
-import { reactive } from "vue";
+import { ref } from "vue";
 
-const cart = reactive({ value: 0 });
+const startQuantity = localStorage.getItem("cartQuantity")
+  ? Number(localStorage.getItem("cartQuantity"))
+  : 0;
+
+const cart = ref(startQuantity);
 
 const addToCart = (quantity: number) => {
   cart.value += quantity;
+  localStorage.setItem("cartQuantity", String(cart.value));
 };
 
 export { cart, addToCart };
