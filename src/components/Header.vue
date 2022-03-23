b6c708bd0779eb184f7ce1b3ece49538b3d2e23d
Tom Hine <tomhine792@gmail.com>
2022-03-23 15:05:21 +0000

    menu icon opens mobile nav, prettier tailwind shuffle

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,21 +3,24 @@ import Logo from "./icons/Logo.vue";
 import Nav from "./Navigation/Nav.vue";
 import MenuIcon from "./icons/MenuIcon.vue";
 import CartQuantity from "./CartQuantity.vue";
+import { openMobileNav } from "../store/mobileNav";
 </script>
 
 <template>
-  <header class="w-full h-17 lg:h-28 flex items-center justify-center">
+  <header class="flex h-17 w-full items-center justify-center lg:h-28">
     <div
-      class="w-full lg:max-w-6xl h-full flex items-center justify-between border-b-2 px-6 lg:px-0"
+      class="flex h-full w-full items-center justify-between border-b-2 px-6 lg:max-w-6xl lg:px-0"
     >
-      <div class="flex items-center gap-4 lg:gap-12 h-full">
-        <MenuIcon />
+      <div class="flex h-full items-center gap-4 lg:gap-12">
+        <button class="lg:hidden" @click="openMobileNav">
+          <MenuIcon />
+        </button>
         <Logo />
         <Nav />
       </div>
       <div class="flex items-center gap-6 lg:gap-10">
         <CartQuantity />
-        <div class="p-0.5 rounded-full hover:bg-brand-orange cursor-pointer">
+        <div class="cursor-pointer rounded-full p-0.5 hover:bg-brand-orange">
           <img src="/images/image-avatar.png" alt="avatar profile image" class="h-8 lg:h-12" />
         </div>
       </div>
