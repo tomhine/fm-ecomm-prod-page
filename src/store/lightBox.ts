07debccc5f7bd6123907ce538ab6cbe274904d77
tomhine792 <tomhine792@outlook.com>
2022-03-23 21:54:42 +0000

    added image number to open lightBox with correct image first

--- src/store/lightBox.ts
+++ src/store/lightBox.ts
@@ -1,7 +1,11 @@
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 
 export const lightBox = reactive({ isOpen: false });
+export const imageNumber = ref(1);
 
-export const openLightBox = () => (lightBox.isOpen = true);
+export const openLightBox = (number: number) => {
+  imageNumber.value = number;
+  lightBox.isOpen = true;
+};
 
 export const closeLightBox = () => (lightBox.isOpen = false);
