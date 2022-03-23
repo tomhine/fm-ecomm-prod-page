b44b4a6602b76b4ee0bb3ff51ca6fd7967feca83
tomhine792 <tomhine792@outlook.com>
2022-03-23 21:55:16 +0000

    removed shadow as checkout button doesn;t use

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex h-14 w-full items-center justify-center gap-3 rounded-lg bg-brand-orange font-semibold text-white shadow-xl hover:opacity-70"
+    class="flex h-14 w-full items-center justify-center gap-3 rounded-lg bg-brand-orange font-semibold text-white hover:opacity-70"
     @click="$emit('buttonClick')"
   >
     <slot />
