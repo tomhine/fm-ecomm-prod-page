c1c77f5ee078f85d959625e7a4e98fa6cae839ef
tomhine792 <tomhine792@outlook.com>
2022-03-23 21:56:19 +0000

    animation component added for fade

---
+++ src/components/FadeInOutTransition.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Transition
+    enter-active-class="transition duration-1000"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition duration-1000"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <slot></slot>
+  </Transition>
+</template>
