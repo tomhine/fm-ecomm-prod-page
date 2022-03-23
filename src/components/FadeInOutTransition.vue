68805ad8aa2fd631468a55d464205091fe15850a
tomhine792 <tomhine792@outlook.com>
2022-03-23 22:48:01 +0000

    tweek animation duration

--- src/components/FadeInOutTransition.vue
+++ src/components/FadeInOutTransition.vue
@@ -2,10 +2,10 @@
 
 <template>
   <Transition
-    enter-active-class="transition duration-1000"
+    enter-active-class="transition duration-200"
     enter-from-class="opacity-0"
     enter-to-class="opacity-100"
-    leave-active-class="transition duration-1000"
+    leave-active-class="transition duration-200"
     leave-from-class="opacity-100"
     leave-to-class="opacity-0"
   >
