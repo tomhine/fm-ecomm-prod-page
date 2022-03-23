64493a5d4e2ad683562f8cbf49f7857d552788eb
tomhine792 <tomhine792@outlook.com>
2022-03-23 19:57:43 +0000

    add disable to minus button when count < 1

--- src/components/ProductQuantity.vue
+++ src/components/ProductQuantity.vue
@@ -30,6 +30,7 @@ const addItemsToCart = () => {
       @click="decrement"
       class="flex h-14 w-14 items-center justify-center text-brand-orange"
       :class="{ 'hover:opacity-60': count >= 1, 'cursor-default': count < 1 }"
+      :disabled="count < 1"
     >
       <MinusIcon />
     </button>
